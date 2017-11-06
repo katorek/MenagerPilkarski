4e39d0c0c1cb92dd5de0fd5c6b038e12398bfd16
katorek <wojtasj1996@gmail.com>
2017-11-06 17:35:11 +0100

    feature-1-setup-the-project Przykladowy model danych

--- src/main/resources/db/migrations/V1.0.1__Init.sql
+++ src/main/resources/db/migrations/V1.0.1__Init.sql
@@ -9,3 +9,15 @@ CREATE TABLE PILKARZE(
   IMIE VARCHAR(30) NOT NULL,
   NAZWISKO VARCHAR(30) NOT NULL
 );
+
+INSERT INTO DRUZYNY (NAZWA) VALUES
+  ('Lech Poznań'),
+  ('Legia Warszawa'),
+  ('Korona Kielce'),
+  ('Real Madryt');
+
+INSERT INTO PILKARZE (IMIE,NAZWISKO) VALUES
+  ('Robert','Lewandowski'),
+  ('Cristiano','Ronaldo'),
+  ('Lionel','Messi'),
+  ('Wojciech','Szczęsny');
